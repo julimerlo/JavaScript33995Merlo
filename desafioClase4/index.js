a0c3b23cb65eb2828f4e7c56a0ea6d7e62931b78
Julian Merlo <merlo_julian@hotmail.com>
2022-09-05 22:38:35 -0300

    correccion desafio clase 4

--- desafioClase4/index.js
+++ desafioClase4/index.js
@@ -1,18 +1,23 @@
-let edad = parseInt(prompt("Ingresa tu edad"));
+let edad;
+
+do {
+  edad = parseInt(prompt("Ingresa tu edad"));
+} while (isNaN(edad));
+
+function calculadora() {
+  let nombre = prompt(
+    "Hola! Bienvenido a la cueva de los dolares. Por favor ingresa tu nombre"
+  );
+  let cotizacionUsd = prompt(
+    `${nombre} ingresa la cotizacion del DOLAR BLUE del dia de hoy`
+  );
+  let consulta = prompt("Cuantos USD quieres comprar???");
+  let pesos = cotizacionUsd * consulta;
+  alert(`${nombre} necesitaras ${pesos} pesos argentinos`);
+}
 
 if (edad < 18) {
   alert("No puedes comprar dolares");
 } else {
-  function calculadora() {
-    let nombre = prompt(
-      "Hola! Bienvenido a la cueva de los dolares. Por favor ingresa tu nombre"
-    );
-    let cotizacionUsd = prompt(
-      `${nombre} ingresa la cotizacion del dolar oficial banco nacion del dia de hoy`
-    );
-    let consulta = prompt("Cuantos USD quieres comprar???");
-    let pesos = cotizacionUsd * consulta;
-    alert(`${nombre} necesitaras ${pesos} pesos argentinos`);
-  }
+  calculadora();
 }
-calculadora();
